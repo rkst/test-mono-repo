6d1224138ddd23736cb2256f841890505d67b66f
Robbie T <github@rkst.it>
2019-01-23 10:37:53 -0800

    Hate dev

--- dev/dev-instances.tf
+++ dev/dev-instances.tf
@@ -2,11 +2,11 @@ provider "aws" {
   region = "${var.region}"
 }
 
-resource "aws_instance" "test1" {
-  ami = "${lookup(var.amis, var.region)}"
-  instance_type = "t1.micro"
-  tags = {
-    Name = "tf-test1"
-    Name2 = "dev"
-  }
-}
+# resource "aws_instance" "test1" {
+#   ami = "${lookup(var.amis, var.region)}"
+#   instance_type = "t1.micro"
+#   tags = {
+#     Name = "tf-test1"
+#     Name2 = "dev"
+#   }
+# }
