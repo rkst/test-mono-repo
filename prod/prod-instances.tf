ef836217ee08925346b09d058eb063e6ac77dd22
Robbie T <github@rkst.it>
2019-02-27 12:29:38 -0800

    Remove all instance

--- prod/prod-instances.tf
+++ prod/prod-instances.tf
@@ -2,11 +2,11 @@ provider "aws" {
   region = "${var.region}"
 }
 
-resource "aws_instance" "prod1" {
-  ami = "${lookup(var.amis, var.region)}"
-  instance_type = "t1.micro"
-  tags = {
-    Name = "tf-prod1"
-    Name2 = "Prod"
-  }
-}
+# resource "aws_instance" "prod1" {
+#   ami = "${lookup(var.amis, var.region)}"
+#   instance_type = "t1.micro"
+#   tags = {
+#     Name = "tf-prod1"
+#     Name2 = "Prod"
+#   }
+# }
--- staging/stage-instances.tf
+++ staging/stage-instances.tf
@@ -2,11 +2,11 @@ provider "aws" {
   region = "${var.region}"
 }
 
-resource "aws_instance" "stage1" {
-  ami = "${lookup(var.amis, var.region)}"
-  instance_type = "t1.micro"
-  tags = {
-    Name = "tf-stage1"
-    Name2 = "Staging"
-  }
-}
+# resource "aws_instance" "stage1" {
+#   ami = "${lookup(var.amis, var.region)}"
+#   instance_type = "t1.micro"
+#   tags = {
+#     Name = "tf-stage1"
+#     Name2 = "Staging"
+#   }
+# }
